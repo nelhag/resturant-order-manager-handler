e1269cd3cbab114c664aca0b02b0a64cc1d98f37
nelhag <lamais2006@yahoo.com>
2019-07-15 15:53:47 -0400

    project setup

---
+++ src/main/java/wcci/resturant/order/manager/handler/ResturantOrderManagerHandlerApplication.java
@@ -0,0 +1,13 @@
+package wcci.resturant.order.manager.handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantOrderManagerHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResturantOrderManagerHandlerApplication.class, args);
+	}
+
+}
